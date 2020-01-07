97aea81c4a43c99df8c77e0d7e017269e5ce0602
AndreasJe <gud.er.gud@gmail.com>
2020-01-07 06:39:22 +0100

    Content færdig - Tid til finpudsning.
    Undlod Redesign, da det var et clusterfuck

--- script.js
+++ script.js
@@ -92,3 +92,30 @@ try {
 } catch (e) {
 
 }
+// Test-box   - Catch error method
+
+
+try {
+  // Slideshow controls
+  var slideIndex = 0;
+  showSlides();
+
+  function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1
+    }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+  }
+
+} catch (e) {
+
+}
+
+// Slider   - Catch error method
